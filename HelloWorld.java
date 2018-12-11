aa7e0280158d0906eb9574e46b6c859254065faa
Andrew S <yipten@outlook.com>
2018-12-10 20:57:59 -0500

    added comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        //prints "Hello world!" to the console
         System.out.println("Hello world!");
+        //does the same thing with "(again)" added to the end
         System.out.println("Hello world! (again)");
     }
 }
